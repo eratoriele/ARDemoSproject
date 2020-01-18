a25b3e1e94f0b4345955a05f4050988f2261e8f4
Bora Gulerman <fafadaw1234@gmail.com>
2020-01-18 01:53:47 -0800

    Initial Commit

---
+++ ARDemoScodeTests/ARDemoScodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARDemoScodeTests.swift
+//  ARDemoScodeTests
+//
+//  Created by macos on 18.01.2020.
+//  Copyright © 2020 macos. All rights reserved.
+//
+
+import XCTest
+@testable import ARDemoScode
+
+class ARDemoScodeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
